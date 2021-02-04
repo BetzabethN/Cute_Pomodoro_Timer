986571a074f285c99b96a60ecc569ab0b16b4d15
Betzabeth Narvaez <betzynarv@gmail.com>
2021-02-04 13:17:34 -0600

    try to fix timer

--- timerScript.js
+++ timerScript.js
@@ -1,5 +1,7 @@
 var minutes;
 var seconds;
+var rminutes;
+var rseconds;
 const p_title =  document.querySelector('#pomodoro-title');
 
 function study() {
@@ -40,8 +42,8 @@ function study() {
 };
 
 function rest() {
-    minutes = 5;
-    seconds = 0;
+    rminutes = 5;
+    rseconds = 0;
     p_title.innerText = "Rest!";
     document.getElementById("#pomodoro-pic").src = "images/rest.png";
 
@@ -49,7 +51,7 @@ function rest() {
         function() {
             var el = document.getElementById("timer")
             // if countdown in done, clear it, and alert
-            if (seconds == 0 && minutes == 0) {
+            if (rseconds == 0 && rminutes == 0) {
                 clearInterval(interval);
                 el.innerHTML = "00:00";
                 el.style.backgroundColor = "red";
@@ -57,19 +59,19 @@ function rest() {
                 document.getElementById("#pomodoro-pic").src = "images/pause-session.png";
             }
             // if seconds is done, minus a minute
-            else if (seconds == 0) {
-                minutes = minutes - 1;
-                seconds = 59;
-                el.innerHTML = minutes + ":" + seconds;
+            else if (rseconds == 0) {
+                rminutes = rminutes - 1;
+                rseconds = 59;
+                el.innerHTML = rminutes + ":" + rseconds;
                 // change doc title so if in another tab, can still see time left
-                document.title = minutes + ":" + seconds;
+                document.title = rminutes + ":" + rseconds;
             }
             // as long as there is seconds left, subtract seconds
-            else if (seconds > 0) {
-                seconds = seconds - 1;
-                el.innerHTML = minutes + ":" + seconds;
+            else if (rseconds > 0) {
+                rseconds = rseconds - 1;
+                el.innerHTML = rminutes + ":" + rseconds;
                 // change doc title so if in another tab, can still see time left
-                document.title = minutes + ":" + seconds;
+                document.title = rminutes + ":" + rseconds;
             }
         },
         1000
