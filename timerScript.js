1f697db368e82c3af64dd7d6ab5b83b943f1757e
Betzabeth Narvaez <betzynarv@gmail.com>
2021-02-04 13:09:32 -0600

    complete redo of index and script

--- timerScript.js
+++ timerScript.js
@@ -1,9 +1,12 @@
 var minutes;
 var seconds;
+const p_title =  document.querySelector('#pomodoro-title');
 
-function studyBreak(x) {
-    minutes = x;
+function study() {
+    minutes = 25;
     seconds = 0;
+    p_title.innerText = "Study or Work!";
+    document.getElementById("#pomodoro-pic").src = "images\in-session.png";
 
     var interval = setInterval(
         function() {
@@ -13,6 +16,45 @@ function studyBreak(x) {
                 clearInterval(interval);
                 el.innerHTML = "00:00";
                 el.style.backgroundColor = "red";
+                p_title.innerText = "Ready to start again?";
+                document.getElementById("#pomodoro-pic").src = "images\pause-session.png";
+            }
+            // if seconds is done, minus a minute
+            else if (seconds == 0) {
+                minutes = minutes - 1;
+                seconds = 59;
+                el.innerHTML = minutes + ":" + seconds;
+                // change doc title so if in another tab, can still see time left
+                document.title = minutes + ":" + seconds;
+            }
+            // as long as there is seconds left, subtract seconds
+            else if (seconds > 0) {
+                seconds = seconds - 1;
+                el.innerHTML = minutes + ":" + seconds;
+                // change doc title so if in another tab, can still see time left
+                document.title = minutes + ":" + seconds;
+            }
+        },
+        1000
+    )
+};
+
+function rest() {
+    minutes = 5;
+    seconds = 0;
+    p_title.innerText = "Rest!";
+    document.getElementById("#pomodoro-pic").src = "images\rest.png";
+
+    var interval = setInterval(
+        function() {
+            var el = document.getElementById("timer")
+            // if countdown in done, clear it, and alert
+            if (seconds == 0 && minutes == 0) {
+                clearInterval(interval);
+                el.innerHTML = "00:00";
+                el.style.backgroundColor = "red";
+                p_title.innerText = "Ready to start again?";
+                document.getElementById("#pomodoro-pic").src = "images\pause-session.png";
             }
             // if seconds is done, minus a minute
             else if (seconds == 0) {
