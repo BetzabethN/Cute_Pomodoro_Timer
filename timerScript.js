ba46168572a9778c8fdd5a2e17fcbbf397e27505
Betzabeth Narvaez <betzynarv@gmail.com>
2021-02-04 12:58:11 -0600

    finish timer function

--- timerScript.js
+++ timerScript.js
@@ -1,25 +1,35 @@
 var minutes;
 var seconds;
 
-function study() {
-    minutes = 25;
+function studyBreak(x) {
+    minutes = x;
     seconds = 0;
 
     var interval = setInterval(
         function() {
             var el = document.getElementById("timer")
-            if (seconds == 0) {
-                minutes = minutes -1;
+            // if countdown in done, clear it, and alert
+            if (seconds == 0 && minutes == 0) {
+                clearInterval(interval);
+                el.innerHTML = "00:00";
+                el.style.backgroundColor = "red";
+            }
+            // if seconds is done, minus a minute
+            else if (seconds == 0) {
+                minutes = minutes - 1;
                 seconds = 59;
                 el.innerHTML = minutes + ":" + seconds;
-            } else if (minutes > 0) {
+                // change doc title so if in another tab, can still see time left
+                document.title = minutes + ":" + seconds;
+            }
+            // as long as there is seconds left, subtract seconds
+            else if (seconds > 0) {
                 seconds = seconds - 1;
                 el.innerHTML = minutes + ":" + seconds;
-
+                // change doc title so if in another tab, can still see time left
+                document.title = minutes + ":" + seconds;
             }
-
         },
         1000
     )
-
-}
+};
